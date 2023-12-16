bb90677b04a233408226fb3ca070f2994a4b0418
nikhildhage <nikhil.dhage@gmail.com>
2023-12-16 04:14:48 -0800

    Fixed little issues and typos in index template

--- FinalProject/settings.py
+++ FinalProject/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, "Restaurant/static"),
+    os.path.join(BASE_DIR / "Restaurant", "static")
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "assets")
