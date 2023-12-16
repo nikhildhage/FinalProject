e9b4015ba02221c6ee1761b1ddd9928c320e7aae
nikhildhage <nikhil.dhage@gmail.com>
2023-12-15 23:10:26 -0800

    Updated Restaurant Views

--- Restaurant/views.py
+++ Restaurant/views.py
@@ -1,35 +1,38 @@
 import os
 
-from django.contrib.auth.models import User
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.utils.dateparse import parse_date
 from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import *
 
-from Restaurant.models import Reservation
+from .models import Reservation
 
 
 # Create your views here.
 
-def yurts(request):
-    return render(request, "yurts.html")
+def index(request):
+    return render(request, "index.html")
 
 
-# Activities View
-def activities(request):
-    return render(request, "activities.html")
+def menu(request):
+    return render(request, "Menus.html")
+
+
+# Locations view
+def locations(request):
+    return render(request, "Locations.html")
 
 
 # Reservations View
 def reservations(request):
-
+    #  # Extracting  form data from reservation form
     if request.method == 'POST':
-        # Extracting form data
         first_name = request.POST.get('myFName')
         last_name = request.POST.get('myLName')
         email = request.POST.get('myEmail')
         phone_number = request.POST.get('myPhone')
         arrival_date = parse_date(request.POST.get('myDate'))
-
+        nights = request.POST.get('myPhone')
         comments = request.POST.get('myComments')
 
         # Create Reservation instance
@@ -39,18 +42,10 @@ def reservations(request):
             email=email,
             phone_number=phone_number,
             arrival_date=arrival_date,
+            nights=nights,
             comments=comments
         )
         reservation.save()  # Save the reservation to the database
-
-        # Create User instance and link it to the reservation
-        user = User(
-            user_firstname=first_name,
-            user_lastname=last_name,
-            user_email=email,
-            reservation=reservation
-        )
-        user.save()  # Save the user to the database
 
         email_content = f"""
                 <strong>Your reservation has been confirmed.</strong><br><br>
@@ -75,12 +70,14 @@ def reservations(request):
             print(response.headers)
         except Exception as e:
             print(e.message)
+    return render(request, "Reservations.html")
 
-        return redirect('details')
 
-    return render(request, "reservations.html")
+# Activities View
+def giftcards(request):
+    return render(request, "GiftCard.html")
 
 
 # Details View
-def details(request):
-    return render(request, "reservationDetails.html")
+def contact(request):
+    return render(request, "Contact.html")
