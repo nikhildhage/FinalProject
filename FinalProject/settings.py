2a89e0f6ee9c1c8995266b2695e42d862f21d53d
nikhildhage <nikhil.dhage@gmail.com>
2023-12-16 23:15:26 -0800

    FConfigured Debug Tool Bar in project

--- FinalProject/settings.py
+++ FinalProject/settings.py
@@ -35,7 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'Restaurant'
+
 ]
 
 MIDDLEWARE = [
@@ -46,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware'
 ]
 
 ROOT_URLCONF = 'FinalProject.urls'
@@ -123,3 +126,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "assets")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    '127.0.0.1'
+]
--- FinalProject/urls.py
+++ FinalProject/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import include, path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import include, path
 from Restaurant import views
 
 urlpatterns = [
@@ -25,4 +26,10 @@ urlpatterns = [
     path("admin/", admin.site.urls),
 ]
 
+# Adds Debug Tool Bar to path
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += path('__debug__/', include(debug_toolbar.urls)),
+
+# Adds staticfiles to path
 urlpatterns += staticfiles_urlpatterns()
